c7c4806dfc73c14b680024cf1fb3ec6943a82dc7
Daniel Pena Berroa <digrape07@gmail.com>
2019-02-16 20:30:22 -0400

    redux devtools

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { ordersReducer } from './store/orders/ordes-reducer';
 import { Provider } from 'react-redux';
 import App from './components/app/App';
 
-const store = createStore(ordersReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(),
+  // other store enhancers if any
+);
+
+const store = createStore(ordersReducer, enhancer);
 
 ReactDOM.render(
     <Provider store={store}>
