6c8d640797ad11d5e5257582b1f57179da34b556
Daniel Pena Berroa <digrape07@gmail.com>
2019-02-16 20:21:10 -0400

    Chrome devtool Redux support

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { ordersReducer } from './store/orders/ordes-reducer';
 import { Provider } from 'react-redux';
 import App from './components/app/App';
 
-const store = createStore(ordersReducer);
+const store = createStore(ordersReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
     <Provider store={store}>
