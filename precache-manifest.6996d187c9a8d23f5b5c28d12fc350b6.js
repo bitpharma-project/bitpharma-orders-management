a8f8b40d95d064a6d692a4f8b9a13a613ed8db61
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-01 19:07:02 +0000

    Deploy notbaddays/bitpharma-orders-management to github.com/notbaddays/bitpharma-orders-management.git:gh-pages

---
+++ precache-manifest.6996d187c9a8d23f5b5c28d12fc350b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fa0e755827a3fff7484",
+    "url": "/static/js/main.4fa0e755.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2443e10ad93461fa24c1",
+    "url": "/static/js/2.2443e10a.chunk.js"
+  },
+  {
+    "revision": "2cff0ca31f8fac3f004c72da1c2c5f8d",
+    "url": "/static/media/logo_dark_bg.2cff0ca3.svg"
+  },
+  {
+    "revision": "8477045c14e8f5c2f144fef399b0883a",
+    "url": "/index.html"
+  }
+];
