310ccbc64ce0d832068ecfee0cb9863d47873017
Daniel Pena Berroa <digrape07@gmail.com>
2019-05-13 23:07:33 -0400

    login screen

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,56 +1,173 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles';
-import { withRouter, Redirect } from 'react-router-dom';
-import logo from '../../assets/img/logo_dark_bg.svg';
+import React from 'react';
+import styled from 'styled-components';
+import EmailIconMui from '@material-ui/icons/Email';
+import LockIconMui from '@material-ui/icons/Lock';
+import NavigateNextIconMui from '@material-ui/icons/NavigateNext';
+import { Redirect } from 'react-router-dom';
+import CircularProgressMui from '@material-ui/core/CircularProgress';
+import axios from 'axios';
+import { ApiServer } from '../../settings';
 import { withCookies } from 'react-cookie';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import withAPI from '../../utils/withAPI';
 
-const styles = theme => ({
-  main: {
-    width: 'auto',
-    display: 'block', // Fix IE 11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 8,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing.unit,
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 3,
-    background: 'linear-gradient(45deg, #201E48 30%, #6C539E 90%)'
-  },
-});
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
 
-class SignIn extends Component {
+const BackContent = styled.div`
+  height: 40%;
+  width: 100%;
+  background-image:
+    linear-gradient(to top, rgb(70, 47, 142, 0.95) 65%, rgb(77, 51, 153, 0.85)),
+    url('burbles-purple.jpg');
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const LogoWrapper = styled.img`
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+  box-shadow: 0px 0px 6px 0px #7053c6;
+  background-size: cover;
+  margin-top: 10%;
+  @media only screen and (min-width: 768px) { 
+    margin-top: 3%;
+  }
+`;
+
+const TitleWrapper = styled.div`
+  margin-top: 5%;
+  color: white;
+  & > span {
+    font-size: 1.25rem;
+    font-weight: 400;
+    font-family: 'Kodchasan', sans-serif;
+  }
+  @media only screen and (min-width: 768px) { 
+    margin-top: 3%;
+  }
+`;
+
+const LoginFormWrapper = styled.div`
+  width: 80%;
+  min-height: 340px;
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0px 2px 4px 0px rgb(0, 0, 0, 0.65);
+  margin: -56px;
+  max-width: 640px;
+  @media only screen and (min-width: 768px) { 
+    padding: 16px;
+  }
+`;
+
+const InputWrapper = styled.div`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  & > svg {
+    position: absolute;
+    left: 8px;
+  }
+  & > input {
+    padding-left: 40px;
+  }
+`;
+
+const Input = styled.input`
+  width: 90%;
+  border: none;
+  box-shadow: none;
+  height: 50px;
+  padding: 8px;
+  outline: none;
+  font-size: 1.0rem;
+  border-bottom: solid 0px rgb(96, 64, 191);
+  transition: border-bottom 0.1s ease-out;
+  &:focus {
+    border-bottom: solid 1.5px rgb(96, 64, 191);
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  margin-top: 24px;
+  & > svg, div {
+    position: absolute;
+    right: 24px;
+    font-size: 1.45rem;
+  }
+`;
+
+const Button = styled.button`
+  border-radius: 4px;
+  box-shadow: 0px 0px 4px 0px #ccc;
+  color: white;
+  background-image: linear-gradient(to right, #5639ac 0%, #7053c6 100%);
+  cursor: pointer;
+  width: 90%;
+  height: 100%;
+  font-size: 1.05rem;
+  @media only screen and (min-width: 768px) { 
+    width: 100%;
+  }
+`;
+
+const NavigateNextIcon = styled(NavigateNextIconMui)`
+  color: white;
+`;
+
+const EmailIcon = styled(EmailIconMui)`
+  color: rgb(77, 51, 153);
+`;
+
+const LockIcon = styled(LockIconMui)`
+  color: rgb(77, 51, 153);
+`;
+
+const WelcomeBackText = styled.div`
+  min-height: 80px; 
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 80px;
+  align-items: center;
+
+  & > span {
+    font-family: 'Kaushan Script', cursive;
+  }
+`;
+
+const ErrorWrapper = styled.div`
+  width: 100%;
+  height: 28px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & > span {
+    font-size: 0.85rem;
+    text-align: center;
+    color: darkred;
+  }
+`;
+
+const CircularProgress = styled(CircularProgressMui)`
+  color: white !important;
+`;
+
+class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -62,18 +179,6 @@ class SignIn extends Component {
       errors: [],
       isLoggedIn: false
     }
-    this.handleOnSubmit = this.handleOnSubmit.bind(this);
-    this.handleUserEmail = this.handleUserEmail.bind(this);
-    this.handleUserPassword = this.handleUserPassword.bind(this);
-    this.handleUserRemember = this.handleUserRemember.bind(this);
-  }
-
-  componentDidMount() {
-    //console.log(this.props);
-  }
-
-  handleUserRemember = (e) => {
-    this.setState({rememberMe: e.target.value});
   }
 
   handleUserEmail = (e) => {
@@ -91,7 +196,7 @@ class SignIn extends Component {
     this.setState({
       isLoading: true
     });
-    API.post('login', {user: {email: email, password: password} })
+    axios.post(`${ApiServer}/login`, {user: {email: email, password: password} })
     .then(data => {
       if (!!data) {
         const tokenReceived = this.getToken(data.headers['authorization']);
@@ -106,6 +211,7 @@ class SignIn extends Component {
         });
       }
     }).catch( (err) => {
+      console.log(err);
       this.setState({
         isLoading: false,
         hasErrors: true,
@@ -128,65 +234,55 @@ class SignIn extends Component {
   }
 
   render() {
-    const { classes } = this.props;
     const { email, password, rememberMe, hasErrors, errors, isLoggedIn, isLoading } = this.state;
 
-    let ErrorComponent = <div>
-      <Typography component="h6" variant="body1">
-        Email or password are wrong. Please, try again.
+    const ErrorComponent = (
+      <ErrorWrapper>
+        <span>Email or password are wrong. Please, try again.</span>
         {errors.map((item, index) => (
           <span key={index}>{item}</span>
         ))}
-      </Typography>
-    </div>
+      </ErrorWrapper>
+    );
+
+    console.log(isLoggedIn);
 
     if (isLoggedIn) return <Redirect to='/orders' />
 
-    return (
-      <main className={classes.main}>
-        <CssBaseline />
-        <Paper className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <img alt="Bit-orders" src={logo} width="48px" height="48px" />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          {
-            hasErrors? ErrorComponent : null
-          }
-          <form className={classes.form}>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="email">Email Address</InputLabel>
-              <Input value={email} onChange={this.handleUserEmail} id="email" name="email" autoComplete="email" autoFocus />
-            </FormControl>
-            <FormControl margin="normal" required fullWidth>
-              <InputLabel htmlFor="password">Password</InputLabel>
-              <Input value={password} onChange={this.handleUserPassword} name="password" type="password" id="password" autoComplete="current-password" />
-            </FormControl>
-            <FormControlLabel
-              control={<Checkbox checked={rememberMe} onChange={this.handleUserRemember} value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-              onClick={this.handleOnSubmit}
-            >
-              Login { isLoading ? <CircularProgress style={{ marginLeft: '7px' }} size={20} color="#fff" /> : null }
-            </Button>
-          </form>
-        </Paper>
-      </main>
+    return(
+      <Wrapper>
+        <BackContent>
+          <LogoWrapper src="logo_dark_bg.svg" alt="Bit orders management | Bitpharma">
+          </LogoWrapper>
+          <TitleWrapper>
+            <span>Bitpharma | Orders management</span>
+          </TitleWrapper>
+        </BackContent>
+        <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+          <LoginFormWrapper>
+            <WelcomeBackText>
+              <span>Welcome back!</span>
+            </WelcomeBackText>
+            {
+              hasErrors? ErrorComponent : null
+            }
+            <InputWrapper>
+              <Input type="email" placeholder="Your email..." value={email} onChange={this.handleUserEmail} id="email" name="email" autoComplete="email" autoFocus />
+              <EmailIcon />
+            </InputWrapper>
+            <InputWrapper>
+              <Input type="password" placeholder="Your password..." value={password} onChange={this.handleUserPassword} name="password" id="password" autoComplete="current-password" />
+              <LockIcon />
+            </InputWrapper>
+            <ButtonWrapper>
+              <Button onClick={this.handleOnSubmit}>LOGIN</Button>
+              { isLoading ? <CircularProgress style={{ marginLeft: '7px' }} size={20} color="#fff" /> : <NavigateNextIcon /> }
+            </ButtonWrapper>
+          </LoginFormWrapper>
+        </div>
+      </Wrapper>
     );
   }
 }
 
-SignIn.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(withRouter(withCookies(withAPI(SignIn))));
+export default withCookies(Login);
