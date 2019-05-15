0d3f179feab9ae2138670ee5cdf000737cb2a52c
Daniel Pena Berroa <digrape07@gmail.com>
2019-05-15 17:49:46 -0400

    prod endposint ssl

--- src/settings.jsx
+++ src/settings.jsx
@@ -1,6 +1,6 @@
-export const ApiServer = process.env.NODE_ENV === 'development' ? 'http://api.bitpharma.xyz/api' : 'https://api.bitpharma.xyz/api';
-export const WSConnection = process.env.NODE_ENV === 'development' ? 'ws://api.bitpharma.xyz' : 'wss://api.bitpharma.xyz';
-export const Server = process.env.NODE_ENV === 'development' ? 'http://api.bitpharma.xyz' : 'https://api.bitpharma.xyz';
+export const ApiServer = process.env.NODE_ENV === 'development' ? 'http://api.bitpharma.xyz/api' : 'http://api.bitpharma.xyz/api';
+export const WSConnection = process.env.NODE_ENV === 'development' ? 'ws://api.bitpharma.xyz' : 'ws://api.bitpharma.xyz';
+export const Server = process.env.NODE_ENV === 'development' ? 'http://api.bitpharma.xyz' : 'http://api.bitpharma.xyz';
 
 // export const ApiServer = process.env.NODE_ENV === 'development' ? 'http://0.0.0.0:3002/api' : 'http://api.bitpharma.xyz/api';
 // export const WSConnection = process.env.NODE_ENV === 'development' ? 'ws://0.0.0.0:3002' : 'ws://api.bitpharma.xyz';
